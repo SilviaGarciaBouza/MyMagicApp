a3ad6f939d1c598789fa9d830410f1ef343242ad
SilviaGarciaBouza <silvia.gar.bou.dev@gmail.com>
2025-09-30 14:46:33 +0200

    added vm

--- app/src/main/java/com/github/mymagicapp/data/viewModel/ViewModel.kt
+++ app/src/main/java/com/github/mymagicapp/data/viewModel/ViewModel.kt
@@ -1,10 +1,36 @@
 package com.github.mymagicapp.data.viewModel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.github.mymagicapp.data.Animal
+import com.github.mymagicapp.data.ZodiacSign
+import com.github.mymagicapp.data.gethoroscopReadingRandon
 
 class ViewModel: ViewModel() {
     var animalSelected by mutableStateOf<Animal?>(null)
     private set
+
+    var signSelected by mutableStateOf<ZodiacSign?>(null)
+        private set
+
+    var sentenceHoroscopeDay by mutableStateOf<String?>(null)
+        private set
+
+
+
+    fun seleccionarAnimal(animal: Animal) {
+        animalSelected = animal
+    }
+
+
+    fun seleccionarSigno(signo: ZodiacSign) {
+        signSelected = signo
+        generHoroscopOfDay(signo)
+    }
+
+    private fun generHoroscopOfDay(signo: ZodiacSign) {
+        sentenceHoroscopeDay = gethoroscopReadingRandon(signo)
+    }
 }
